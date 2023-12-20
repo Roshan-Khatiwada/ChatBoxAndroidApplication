42f2616e53c4d0c79fe81c701817cb678480b24c
gaurav20basnet <gauravgauravbasnet@gmail.com>
2023-12-20 18:02:35 +0545

    first commit

---
+++ app/src/main/java/com/example/chatbox/Splash_Screen.java
@@ -0,0 +1,39 @@
+package com.example.chatbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class Splash_Screen extends AppCompatActivity {
+
+private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        textView=findViewById(R.id.textView3);
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.anim);
+        textView.setAnimation(anim);
+
+        new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent intent=new Intent(Splash_Screen.this,MainActivity.class);
+                startActivity(intent);
+                Splash_Screen.this.finish();
+
+            }
+        }.start();
+    }
+}
